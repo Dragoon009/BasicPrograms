96f510adb1d70bc3d603c7c830971e374705953d
Dragoon009 <31214703+Dragoon009@users.noreply.github.com>
2017-08-23 18:29:11 +0530

    Update and rename cvs.rb to Read CSV file, multiple 2 columns into third.rb

--- MyPrograms/Read CSV file, multiple 2 columns into third.rb
+++ MyPrograms/Read CSV file, multiple 2 columns into third.rb
@@ -1,26 +1,21 @@
 #!/usr/bin/ruby
 
 require 'csv'
-
 multiplying_array = Array.new
-
-
 CSV.foreach("/home/rails/test2.csv" , converters: :numeric) do |row|
     multiplying_array<< row[1]*row[0]
  end
-
-
 Dummy_array = CSV.read("/home/rails/test2.csv")
-
 Dummy_array.each do|row|
   row << multiplying_array.shift
 end
-
-
 CSV.open("/home/rails/test2.csv" , 'w') do |column3|
   Dummy_array.each { |r|
   column3 << r }
 end
-#Read from a CSV file, multiple two columns, and then write back to the CSV file.
+CSV.foreach("/home/rails/test2.csv") do |row|
+    print row
+end
+#["1", "2", "2"]["3", "4", "12"]["5", "6", "30"]["7", "8", "56"]
 
 
