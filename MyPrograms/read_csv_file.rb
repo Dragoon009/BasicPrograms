f6aab4287c1a85a7f9170a982d3c5d48ee5fd317
Dragoon009 <31214703+Dragoon009@users.noreply.github.com>
2017-08-23 20:11:05 +0530

    Update and rename Read CSV file, multiple 2 columns into third.rb to read_csv_file.rb

--- MyPrograms/read_csv_file.rb
+++ MyPrograms/read_csv_file.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/ruby
-
+#Read CSV file, multiple 2 columns into third
 require 'csv'
 multiplying_array = Array.new
 CSV.foreach("/home/rails/test2.csv" , converters: :numeric) do |row|
-    multiplying_array << row[1]*row[0]
+  multiplying_array << row[1]*row[0]
 end
 Dummy_array = CSV.read("/home/rails/test2.csv")
 Dummy_array.each do|row|
@@ -14,7 +14,7 @@ CSV.open("/home/rails/test2.csv" , 'w') do |column3|
   column3 << r }
 end
 CSV.foreach("/home/rails/test2.csv") do |row|
-    print row
+   print row
 end
 #["1", "2", "2"]["3", "4", "12"]["5", "6", "30"]["7", "8", "56"]
 
