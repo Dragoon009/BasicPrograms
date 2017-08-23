8cd7237a70e8c322dcc8ea4dfb46508b14323ffc
Dragoon009 <31214703+Dragoon009@users.noreply.github.com>
2017-08-23 20:43:25 +0530

    Update map_to_double_elements.rb

--- MyPrograms/map_to_double_elements.rb
+++ MyPrograms/map_to_double_elements.rb
@@ -1,8 +1,15 @@
 #!/usr/bin/ruby
 #map to double all elements in array
 sample_array = [1,2,3,4,5]
-sample_array.map!{|num| num * 2}
-print sample_array
+class Mapping{
+  def map_method(array){
+    sample_array.map!{|num| num * 2}
+    print sample_array
+    }
+  }
+}  
+dummy_array = Mapping.new
+dummy_array.map_array(sample_array)
 #[2, 4, 6, 8, 10]
 
 
