b0b58f87dbc415761facfe31f4f7caffd14105ef
Dragoon009 <31214703+Dragoon009@users.noreply.github.com>
2017-08-23 18:30:52 +0530

    Update and rename map.rb to map to double all elements in array.rb

--- MyPrograms/map to double all elements in array.rb
+++ MyPrograms/map to double all elements in array.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 sample_array = [1,2,3,4,5]
 sample_array.map! { |num| num * 2 }
-puts sample_array
-#the map function to double all the elements in the array.
+print sample_array
+#[2, 4, 6, 8, 10]
+
 
