f7d4c418ac27d72cb34f8c7e322800c2bf43f4ab
Dragoon009 <31214703+Dragoon009@users.noreply.github.com>
2017-08-23 19:14:37 +0530

    Update Using hash table, print the frequency of occurence of each character inside an array.rb

--- MyPrograms/Using hash table, print the frequency of occurence of each character inside an array.rb
+++ MyPrograms/Using hash table, print the frequency of occurence of each character inside an array.rb
@@ -1,11 +1,8 @@
 #!/usr/bin/ruby
 Sample_array = %w[ fcb rma fcb ars dor]
-
 count = Hash.new(0)
-
 Sample_array.each do |n|
 count[n] += 1
 end
-
 puts count
 #{"fcb"=>2, "rma"=>1, "ars"=>1, "dor"=>1}
