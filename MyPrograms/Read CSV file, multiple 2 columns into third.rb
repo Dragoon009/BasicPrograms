5e65eba57eb8799c33923d6e3ae5639766079059
Dragoon009 <31214703+Dragoon009@users.noreply.github.com>
2017-08-23 19:14:09 +0530

    Update Read CSV file, multiple 2 columns into third.rb

--- MyPrograms/Read CSV file, multiple 2 columns into third.rb
+++ MyPrograms/Read CSV file, multiple 2 columns into third.rb
@@ -3,7 +3,7 @@
 require 'csv'
 multiplying_array = Array.new
 CSV.foreach("/home/rails/test2.csv" , converters: :numeric) do |row|
-    multiplying_array<< row[1]*row[0]
+    multiplying_array << row[1]*row[0]
 end
 Dummy_array = CSV.read("/home/rails/test2.csv")
 Dummy_array.each do|row|
