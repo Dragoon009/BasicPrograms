3b2daa7fec79bba5a524ec012959f0b1497d9aba
Dragoon009 <31214703+Dragoon009@users.noreply.github.com>
2017-08-23 20:29:09 +0530

    Update join_2_arrays_without_using_inbuilt_functions.rb

--- MyPrograms/join_2_arrays_without_using_inbuilt_functions.rb
+++ MyPrograms/join_2_arrays_without_using_inbuilt_functions.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/ruby
-Class Array{
+Class Joining{
   def function(array1[], array2[])
   array1 += array2
   print array1
   end
   }  
-array1 = ["some", "thing"]
-array2 = ["another", "10"]
-function(array1[],array2[])
+array_1 = ["some", "thing"]
+array_2 = ["another", "10"]
+function(array_1[],array_2[])
 
 #["some", "thing", "another", "10"]
